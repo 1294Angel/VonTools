b4dfaf32d3b28f17af38efe86c059677d497ab40
Vona <46300673+1294Angel@users.noreply.github.com>
2024-10-17 03:05:18 +0100

    Update von_vrctools.py

--- von_vrctools.py
+++ von_vrctools.py
@@ -1,17 +1,31 @@
-import bpy, os # type: ignore
-from pathlib import Path
+import bpy, os,json # type: ignore
 from . import von_createcontrols
 
 
-def get_addon_directory():
-    # Get the directory of the current Python file
+def get_directory():
     addon_directory = os.path.dirname(__file__)
     return addon_directory
 
 def gatherheirarchydata():
-    directory_path = get_addon_directory()
-    print(directory_path    )
+    directory_path = get_directory() + "/Libraries/BoneNames"
+        #OUTPUT - C:\Users\chris\AppData\Roaming\Blender Foundation\Blender\4.2\scripts\addons\VonTools
     for filename in os.listdir(directory_path):
-        file_path = os.path.join(directory_path, filename)  # Create the full file path
-        if os.path.isfile(file_path):  # Check if it is a file
-            print(f"File: {filename}")  # Process the file (e.g., print its name)sdawsd
+        if filename.endswith('.json'):
+            filepath = os.path.join(directory_path, filename)
+            
+            with open(filepath, 'r') as json_file:
+                try:
+                    data = json.load(json_file)
+                    print(f"\nProcessing file: {filename}")
+                    
+                    #If it's a dict then do X (Idiot Proofing)
+                    if isinstance(data, dict):
+                        iterate_over_items(data)
+                    else:
+                        print(f"Skipping file {filename} as it doesn't contain a valid JSON object.")
+                except json.JSONDecodeError as e: # IF ALL FAILS, IDIOT PROOFING
+                    print(f"Error reading {filename}: {e}")
+
+def iterate_over_items(data):
+    for key, value in data.items():
+        print(f"Key: {key}, Value: {value}")
