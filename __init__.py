aecc9a51e33d7f961a037648625d0a5993cd4b3f
Vona <46300673+1294Angel@users.noreply.github.com>
2024-12-13 16:27:04 +0000

    Fixing Location Processing and Error Reporting

--- __init__.py
+++ __init__.py
@@ -3,19 +3,16 @@ import os
 import sys
 
 from . import (
-    von_createcontrols,
     von_menu_popup,
-    von_buttoncontrols
 )
-from .von_menu_popup import (von_menupopup_register, von_menupopup_unregister)
 
 ADDON_FOLDER_PATH = os.path.dirname(__file__)
 
 bl_info = {
     "name": "Vona's Blender Tools",
     "author": "Vona",
-    "version": (0, 1, 1),
-    "blender": (4, 2, 1),
+    "version": (0, 1, 2),
+    "blender": (4, 3, 1),
     "location": "Where the user can find it",
     "description": "Gold Version of Vona's addon that adds tools to expand blenders toolset.",
     "warning": "",
@@ -26,11 +23,10 @@ bl_info = {
 
 
 def register():
-    print(f'Init Folder initiated')
-    von_menupopup_register()
+    von_menu_popup.von_menupopup_register()
     sys.path.append(ADDON_FOLDER_PATH)
 def unregister():
-    von_menupopup_unregister()
+    von_menu_popup.von_menupopup_unregister()
 
 if __name__ == "__main__":
-    von_menupopup_register()
+    von_menu_popup.von_menupopup_register()
